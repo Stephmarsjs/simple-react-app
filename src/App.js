3228d12f36d4428479f0596cf3709efafb94e9ef
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-04-03 17:10:20 -0400

    added container, header, and a card

--- src/App.js
+++ src/App.js
@@ -17,7 +17,21 @@ componentDidMount() {
 }
   render() {
     return (
-      <p>Hello World!</p>
+      <div className="container">
+        <div className="jumbotron">
+          <h1 class="display-4">Blog posts</h1>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            Featured
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
